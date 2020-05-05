e1a7fad2d369c8e8ead0394a085fe9c7948b01a5
napiorkowskik83 <napiorkowskik83@gmail.com>
2020-05-05 23:22:55 +0200

    First unit test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,34 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        //Calculator methods tests
+
+        Calculator calc = new Calculator();
+
+        if (calc.addAtoB(4, 7) == 4+7){
+            System.out.println("addAtoB method test - OK");
+        }else{
+            System.out.println("addAtoB method test - failed");
+        }
+
+        if (calc.substractBfromA(33, 8) == 33-8){
+            System.out.println("substractBfromA method test - OK");
+        }else{
+            System.out.println("substractBfromA method test - failed");
+        }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+
+    public int addAtoB(int a, int b){
+        return a+b;
+    }
+    public int substractBfromA(int a, int b){
+        return a-b;
+    }
+}
